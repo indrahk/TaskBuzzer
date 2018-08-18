0646d939f718977d69d8133dd13f93c6794d7678
indrahk <hkindra.zues@gmai.com>
2018-08-18 21:31:46 +0200

    commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# GLBuzzer
+# GLOBuzzer
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.4.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+import { CartComponent } from './cart/cart.component';
+import { FinishComponent } from './finish/finish.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: 'shopping', component: ShoppingComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'finish', component: FinishComponent },
+  { path: '', component : ShoppingComponent },
+  { path: '**', component : PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingComponent,
+    CartComponent,
+    FinishComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
